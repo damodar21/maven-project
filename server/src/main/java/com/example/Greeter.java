19bf658567463afeb2daed4232c122022fc62047
damodar21 <damodar.sunani@gmail.com>
2020-03-10 10:11:18 -0400

    warning fixed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,11 @@ public class Greeter {
   }
 
 
-  /** This is test program to check if import junit.framework.TestCase;
-  is working fine with the github
+  /** This is test program to check if import junit.framework.TestCase
+  is working fine with the github.
   */
 
-  public final String greet(final String someone) {
+  public final String greet(final String @someone) {
     return String.format("Hello, %s!", someone);
   }
 }
