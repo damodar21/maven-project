7aa04aec11ed45845e2875d8cbf4fbddce0fdc7f
damodar21 <damodar.sunani@gmail.com>
2020-03-10 10:23:13 -0400

    warning fixed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
   is working fine with the github.
   */
 
-  public final String greet(final String @someone) {
+  public String greet(final String @someone) {
     return String.format("Hello, %s!", someone);
   }
 }
